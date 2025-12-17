f9d9332699cb819ebce01784a95baf0961c89f3b
alchemicjon <jrichmond124@gmail.com>
2025-12-17 09:22:49 -0500

    Use lspconfig to set up language servers

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {},
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require('mason-lspconfig').setup({
+        ensure_installed = { "lua_ls", "tsserver" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      -- vim.lsp.config("lua_ls", {})
+      -- vim.lsp.config("ts_ls", {})
+      vim.lsp.enable("lua_ls")
+      vim.lsp.enable("ts_ls")
+      -- config options in after/lsp
+      vim.lsp.enable('ruby-lsp')
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  },
+}
